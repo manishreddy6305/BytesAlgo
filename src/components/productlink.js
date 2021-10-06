ede8fa3491d75e8129a5a23c106cd5f126aaed21
Manish Reddy <76482822+manishreddy6305@users.noreply.github.com>
2021-10-06 15:39:02 +0530

    Add files via upload

---
+++ src/components/productlink.js
@@ -0,0 +1,10 @@
+import './../styles/productlink.css'
+import Navbar2 from './navbar2';
+function Productlink(){
+    return(
+    <>
+        <Navbar2 />
+    </>
+    )
+}
+export default Productlink;
